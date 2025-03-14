a746de4c2ff20d98a0df508d0b4ba213108a1f25
Tehseen316 <tehseena316@gmail.com>
2025-03-14 20:47:33 +0500

    Added Products page, separated Navbar, and created Suppliers page

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,4 +1,12 @@
 
+fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar-container").innerHTML = data;
+            console.log("Navbar loaded");
+        })
+        .catch(error => console.error("Error loading navbar:", error));
+
 const ctx = document.getElementById("salesChart").getContext("2d");
 
 new Chart(ctx, {
